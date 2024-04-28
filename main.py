54972af10788248d1d91c2818c82b0cdc1c295f2
ihaback <ihaback@fastmail.com>
2024-04-28 09:38:37 +0200

    introduce number of words function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    number_of_words = get_number_of_words(text)
+    print(number_of_words)
 
+def get_number_of_words(text):
+    words = text.split()
+    return len(words)
 
 def get_book_text(path):
     with open(path) as f:
