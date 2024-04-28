b3adaae15e30ea905344786c8f5cd255eb2dea50
ihaback <ihaback@fastmail.com>
2024-04-28 10:56:00 +0200

    implement report

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ def main():
     text = get_book_text(book_path)
     number_of_words = get_number_of_words(text)
     character_count_summary = get_character_count_summary(text)
-    print(character_count_summary)
+    character_list = get_character_list(character_count_summary)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{number_of_words} words found in the document")
+    print("")
+    for char in character_list:
+        print(f"The '{char["name"]}' character was found {char["num"]} times")
+    print("--- End report ---")
+
+def sort_on(dict):
+    return dict["num"]
 
 def get_number_of_words(text):
     words = text.split()
@@ -18,6 +28,14 @@ def get_character_count_summary(text):
         else:
             character_count_summary[char] = 1  
     return character_count_summary
+
+def get_character_list(character_count_summary):
+    character_list = []
+    for char in character_count_summary:
+        if char.isalpha():
+            character_list.append({"name": char, "num": character_count_summary[char]})
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
 
 def get_book_text(path):
     with open(path) as f:
